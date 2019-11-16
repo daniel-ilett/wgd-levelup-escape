2c7020661549b4396bb7550ac22be8e74a029c55
daniel-ilett <danielilett@gmail.com>
2019-11-16 11:46:22 +0000

    Added script to drive animations.

---
+++ Assets/Scripts/PlayerMech.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMech : MonoBehaviour
+{
+    // State variables.
+    private bool isMech = false;
+
+    // Component caches.
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(isMech)
+            {
+                anim.SetTrigger("Robotify");
+            }
+            else
+            {
+                anim.SetTrigger("Tankify");
+            }
+
+            isMech = !isMech;
+        }
+    }
+}
